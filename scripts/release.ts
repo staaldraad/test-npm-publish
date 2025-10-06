10c88e03cc74d0781e492895dfa9b5ddc41975d9
Etienne Stalmans <etienne@supabase.io>
2025-10-06 11:01:58 +0200

    chore: publish

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process'
 
-const publishCommand = `npm publish --provenance --tag canary`
+const publishCommand = `npm publish --tag canary`
 
 execSync(publishCommand, { stdio: 'inherit' })
